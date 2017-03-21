a3bfebb530ab8bad89250210119cb632dc133150
Asia Kane and kat gifford <asia@>
2017-03-21 10:17:36 -0700

    add buttons to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
 
+//Walrus Toggles//
 $("p").click(function(){
   $(".initially-hidden").slideToggle();
   $(".initially-showing").slideToggle();
 });
 
+//Background Color Add and Remove Classes//
+
+$("button#red-button").click( function(){
+  $("body").removeClass();
+  $("body").addClass("red-background");
+});
+
+$("button#blue-button").click( function(){
+  $("body").removeClass();
+  $("body").addClass("blue-background");
+});
+
+$("button#green-button").click( function(){
+  $("body").removeClass();
+  $("body").addClass("green-background");
+});
+
+$("button#neutral-button").click( function(){
+  $("body").removeClass();
+  $("body").addClass("neutral-background");
+});
 
 });
